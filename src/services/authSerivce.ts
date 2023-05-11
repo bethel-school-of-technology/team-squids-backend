46d8ec8515274bf65a062df951f64fdcd3186ada
LeeMoTakito <110348661+LeeMoTakito@users.noreply.github.com>
2023-05-11 14:33:19 -0700

    change user to church

--- src/services/authSerivce.ts
+++ src/services/authSerivce.ts
@@ -25,7 +25,7 @@ export const verifyUser = async (req: Request) => {
       // Verify the token and get the user
       try {
           let decoded: any = await jwt.verify(token, secret);
-          return Church.findByPk(decoded.userId);
+          return Church.findByPk(decoded.churchId);
       }
       catch (err) {
           return null;
