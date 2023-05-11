b4a794fbefc66b98b1cb28a696bdd7efb0fb2f51
LeeMoTakito <110348661+LeeMoTakito@users.noreply.github.com>
2023-05-11 14:26:53 -0700

    church model

---
+++ src/services/authSerivce.ts
@@ -0,0 +1,37 @@
+import { Request } from 'express';
+import jwt from 'jsonwebtoken';
+import { Church } from '../module/church';
+
+
+const secret = 'Tea, Earl Grey, Hot';
+
+export const signChurchToken = async (church: Church) => {
+  let token = jwt.sign(
+      { churchId: church.churchId },
+      secret,
+      { expiresIn: '1hr' }
+  );
+  
+  return token;
+}
+export const verifyUser = async (req: Request) => {
+  // Get the Authorization header from the request
+  const authHeader = req.headers.authorization;
+
+  // If header exists, parse token from `Bearer <token>`
+  if (authHeader) {
+      const token = authHeader.split(' ')[1];
+
+      // Verify the token and get the user
+      try {
+          let decoded: any = await jwt.verify(token, secret);
+          return Church.findByPk(decoded.userId);
+      }
+      catch (err) {
+          return null;
+      }
+  }
+  else {
+      return null;
+  }
+}
