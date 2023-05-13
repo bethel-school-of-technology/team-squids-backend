1a19604c134da553c79fded7cda659a36fd4cd8e
RylanTech <rylanworkman9@gmail.com>
2023-05-13 10:54:06 -0400

    Adding the eventConroller, it is not tested though.

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,15 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { db } from './modules';
-import eventRoutes from './routes/eventRoutes'
-import churchRoutes from './routes/churchRoutes'
+import { db } from './module';
 
 const app = express();
-
-app.use(express.json())
 
 function helloWorld(req: Request, res: Response, next: NextFunction){
   res.send('Hello World')
 }
 
-app.use('/api/events', eventRoutes)
-app.use('/api/church', churchRoutes)
 app.use('/', helloWorld);
+
+
 
 db.sync({ alter:true }).then(() => {
   console.info("Connected to the database!")
---
+++ src/module/index.ts
@@ -0,0 +1,20 @@
+import { Sequelize } from "sequelize";
+import { ChurchFactory } from "./church";
+import { AssociateChurchEvent, EventFactory } from "./event";
+
+const dbName = 'ChurchDB';
+const username = 'root';
+const password = 'Password1!';
+
+const sequelize = new Sequelize(dbName, username, password, {
+    host: '127.0.0.1',
+    port: 3306,
+    dialect: 'mysql'
+});
+
+ChurchFactory(sequelize);
+EventFactory(sequelize);
+AssociateChurchEvent();
+
+
+export const db = sequelize;
