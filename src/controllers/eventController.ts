088e0b0699a36af59f4c98075a16f33845ae0479
akinsman22 <hearttorejoice@gmail.com>
2023-05-20 09:44:49 -0700

    uncommented event model,controller,& routes

--- src/controllers/eventController.ts
+++ src/controllers/eventController.ts
@@ -1,110 +1,110 @@
-// import { RequestHandler } from "express";
-// import { verifyUser } from "../services/authSerivce";
-// import { Event } from "../models/event";
+import { RequestHandler } from "express";
+import { verifyUser } from "../services/authSerivce";
+import { Event } from "../models/event";
 
-// export const getAllEvents: RequestHandler = async (req, res, next) => {
-//     //Basic return of all events
-//     let EventsInDB: Event[] = await Event.findAll();
-//     res.json(EventsInDB)
-// }
+export const getAllEvents: RequestHandler = async (req, res, next) => {
+    //Basic return of all events
+    let EventsInDB: Event[] = await Event.findAll();
+    res.json(EventsInDB)
+}
 
-// export const getEvent: RequestHandler = async (req, res, next) => {
-//     let eventId = req.params.eventId
-//     let foundEvent = await Event.findByPk(eventId)
+export const getEvent: RequestHandler = async (req, res, next) => {
+    let eventId = req.params.eventId
+    let foundEvent = await Event.findByPk(eventId)
 
-//     //Finding if the requested event object exists, then sending it
-//     if (foundEvent) {
-//         res.status(200).json(foundEvent);
-//     } else {
-//         res.status(404).json();
-//     }
-// }
+    //Finding if the requested event object exists, then sending it
+    if (foundEvent) {
+        res.status(200).json(foundEvent);
+    } else {
+        res.status(404).json();
+    }
+}
 
-// export const getTenEvents: RequestHandler = async (req, res, next) => {
-//     //searching for events and returning 10
+export const getTenEvents: RequestHandler = async (req, res, next) => {
+    //searching for events and returning 10
 
-//     //Getting this allows us to search
-//     const { Op } = require('sequelize');
+    //Getting this allows us to search
+    const { Op } = require('sequelize');
 
-//     //converts the query to string so we can use it to search
-//     let query = req.params.query.toString()
+    //converts the query to string so we can use it to search
+    let query = req.params.query.toString()
 
-//     //Searching all event objects with matching parameters
-//     let foundEvents = await Event.findAll({
-//         limit: 10,
-//         offset: 0,
-//         //This is the part where it is comparing \/
-//         where: {
-//             [Op.or]: [
-//                 { eventTitle: { [Op.like]: `%${query}%` } },
-//                 { eventAddress: { [Op.like]: `%${query}%` } },
-//                 { org: { [Op.like]: `%${query}%` } }
-//             ]
-//         }
-//     })
-//     if (foundEvents) {
-//         res.status(200).json(foundEvents);
-//     } else {
-//         res.status(404).json();
-//     }
-// }
+    //Searching all event objects with matching parameters
+    let foundEvents = await Event.findAll({
+        limit: 10,
+        offset: 0,
+        //This is the part where it is comparing \/
+        where: {
+            [Op.or]: [
+                { eventTitle: { [Op.like]: `%${query}%` } },
+                { eventAddress: { [Op.like]: `%${query}%` } },
+                { org: { [Op.like]: `%${query}%` } }
+            ]
+        }
+    })
+    if (foundEvents) {
+        res.status(200).json(foundEvents);
+    } else {
+        res.status(404).json();
+    }
+}
 
-// export const createEvent: RequestHandler = async (req, res, next) => {
-//     let verified = await verifyUser(req);
+export const createEvent: RequestHandler = async (req, res, next) => {
+    let verified = await verifyUser(req);
 
-//     if (verified) {
-//         //If thie user is verified and if the event has every required parameter, it will create a new event
-//         let newEvent: Event = req.body;
-//         if (newEvent.eventTitle && newEvent.churchId && newEvent.eventDate && newEvent.eventAddress && newEvent.eventType && newEvent.description) {
-//             let created = await Event.create(newEvent);
-//             res.status(201).json(created);
-//         } else {
-//             res.status(400).json();
-//         }
-//     } else {
-//         res.status(403).json()
-//     }
+    if (verified) {
+        //If thie user is verified and if the event has every required parameter, it will create a new event
+        let newEvent: Event = req.body;
+        if (newEvent.eventTitle && newEvent.churchId && newEvent.eventDate && newEvent.eventAddress && newEvent.eventType && newEvent.description) {
+            let created = await Event.create(newEvent);
+            res.status(201).json(created);
+        } else {
+            res.status(400).json();
+        }
+    } else {
+        res.status(403).json()
+    }
 
 
-// }
+}
 
-// export const updateEvent: RequestHandler = async (req, res, next) => {
-//     let eventId = req.params.eventId;
-//     let editedEvent: Event = req.body;
+export const updateEvent: RequestHandler = async (req, res, next) => {
+    let eventId = req.params.eventId;
+    let editedEvent: Event = req.body;
 
-//     //This grabs the id from the params and makes it so it can be read in the if statement incase the request does not include the id.
-//     let eventIdNum = parseInt(eventId);
+    //This grabs the id from the params and makes it so it can be read in the if statement incase the request does not include the id.
+    let eventIdNum = parseInt(eventId);
 
-//     let matchingEvent = await Event.findByPk(eventId)
+    let matchingEvent = await Event.findByPk(eventId)
 
-//     //If the event that was requested has all of these attributes, edit the event
-//     if (matchingEvent && matchingEvent.eventId ==
-//         eventIdNum && editedEvent.eventTitle && editedEvent.churchId && editedEvent.eventAddress && editedEvent.description && editedEvent.eventDate && editedEvent.eventTime
-//         && editedEvent.eventType) {
-//         await Event.update(editedEvent, { where: {eventId: eventId} })
-//         res.status(200).json();
-//     } else {
-//         res.status(400).json()
-//     }
-// }
+    //If the event that was requested has all of these attributes, edit the event
+    if (matchingEvent && matchingEvent.eventId ==
+        eventIdNum && editedEvent.eventTitle && editedEvent.churchId && editedEvent.eventAddress && editedEvent.description && editedEvent.eventDate && editedEvent.eventTime
+        && editedEvent.eventType) {
+        await Event.update(editedEvent, { where: {eventId: eventId} })
+        res.status(200).json();
+    } else {
+        res.status(400).json()
+    }
+}
 
-// export const deleteEvent: RequestHandler = async (req, res, next) => {
-//     let verified = await verifyUser(req);
+export const deleteEvent: RequestHandler = async (req, res, next) => {
+    let verified = await verifyUser(req);
 
-//     if (verified) {
-//         let eventId = req.params.eventId;
-//         let foundEvent = await Event.findByPk(eventId);
+    if (verified) {
+        let eventId = req.params.eventId;
+        let foundEvent = await Event.findByPk(eventId);
 
-//         //if the user is verified and the event is found, delete it
-//         if (foundEvent) {
-//             await Event.destroy({
-//                 where: { eventId: eventId }
-//             });
-//             res.status(200).json();
-//         } else {
-//             res.status(404).json();
-//         }
-//     } else {
-//         res.status(403).json()
-//     }
-// }
+        //if the user is verified and the event is found, delete it
+        if (foundEvent) {
+            await Event.destroy({
+                where: { eventId: eventId }
+            });
+            res.status(200).json();
+        } else {
+            res.status(404).json();
+        }
+    } else {
+        res.status(403).json()
+    }
+}
--- src/routes/eventRoutes.ts
+++ src/routes/eventRoutes.ts
@@ -1,13 +1,13 @@
-// import { Router } from 'express';
-// import { createEvent, deleteEvent, getAllEvents, getEvent, getTenEvents, updateEvent } from '../controllers/eventController';
+import { Router } from 'express';
+import { createEvent, deleteEvent, getAllEvents, getEvent, getTenEvents, updateEvent } from '../controllers/eventController';
 
-// const router = Router();
+const router = Router();
 
-// router.get("/", getAllEvents)
-// router.get("/:eventId", getEvent)
-// router.get('/searchEvents', getTenEvents)
-// router.post("/", createEvent)
-// router.put('/editevent', updateEvent)
-// router.delete("/:eventId", deleteEvent)
+router.get("/", getAllEvents)
+router.get("/:eventId", getEvent)
+router.get('/searchEvents', getTenEvents)
+router.post("/", createEvent)
+router.put('/editevent', updateEvent)
+router.delete("/:eventId", deleteEvent)
 
 // export default router;
