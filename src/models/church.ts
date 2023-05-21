69934a5b6fd38b2c053d6db619424d08340e9a7a
Jeff Ordway <jeffordway@Jeffs-iMac.local>
2023-05-20 18:18:06 -0700

    fixed church model

--- src/models/church.ts
+++ src/models/church.ts
@@ -1,96 +1,117 @@
-import { DataTypes, InferAttributes, InferCreationAttributes, Model, Sequelize } from "sequelize";
+import {
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+  Sequelize,
+} from "sequelize";
 import { ChurchUser } from "./churchUser";
 
-export class Church extends Model<InferAttributes<Church>, InferCreationAttributes<Church>>{
-    
-    declare churchId: number;
-    declare userId : number;
-    declare churchName: string;
-    declare street: string;
-    declare city: string;
-    declare state: string;
-    declare zip: string;
-    declare phoneNumber: string;
-    declare welcomeMessage: string;
-    declare serviceTime: string;
-    declare imageUrl: string;
-    declare website: string;
-    declare createdAt?: Date;
-    declare updatedAt?: Date;
+export class Church extends Model<
+  InferAttributes<Church>,
+  InferCreationAttributes<Church>
+> {
+  declare churchId: number;
+  declare userId: number;
+  declare churchName: string;
+  declare denomination: string;
+  declare street: string;
+  declare city: string;
+  declare state: string;
+  declare zip: string;
+  declare phoneNumber: string;
+  declare email: string;
+  declare welcomeMessage: string;
+  declare serviceTime: string;
+  declare imageUrl: string;
+  declare website: string;
+  declare createdAt?: Date;
+  declare updatedAt?: Date;
 }
 export function ChurchFactory(sequelize: Sequelize) {
-  Church.init({
+  Church.init(
+    {
       churchId: {
-          type: DataTypes.INTEGER,
-          autoIncrement: true,
-          primaryKey: true,
-          allowNull: false
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
       },
       userId: {
         type: DataTypes.INTEGER,
-        allowNull: false
-    },
-      churchName: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          unique: true
+        allowNull: false,
       },
-      
+      churchName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      denomination: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
       street: {
-          type: DataTypes.STRING,
-          allowNull: false
+        type: DataTypes.STRING,
+        allowNull: false,
       },
       city: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
       },
       state: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
       },
       zip: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
       },
       phoneNumber: {
-          type: DataTypes.STRING,
-          allowNull: false
-      },
-      welcomeMessage:{
         type: DataTypes.STRING,
-        allowNull: true 
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      welcomeMessage: {
+        type: DataTypes.STRING,
+        allowNull: true,
       },
       serviceTime: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
       },
       imageUrl: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
       },
       website: {
         type: DataTypes.STRING,
-        allowNull: true
+        allowNull: true,
       },
-
       createdAt: {
-          type: DataTypes.DATE,
-          allowNull: false,
-          defaultValue: DataTypes.NOW,
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
       },
       updatedAt: {
-          type: DataTypes.DATE,
-          allowNull: false,
-          defaultValue: DataTypes.NOW,
-      }
-  }, {
-      tableName: 'church',
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    {
+      tableName: "church",
       freezeTableName: true,
-      sequelize
-  });
+      sequelize,
+    }
+  );
 }
 
-export function AssociateUserChurch(){
-  ChurchUser.hasMany(Church, { foreignKey: 'userId' });
-  Church.belongsTo(ChurchUser, { foreignKey: 'userId' });
+export function AssociateUserChurch() {
+  ChurchUser.hasMany(Church, { foreignKey: "userId" });
+  Church.belongsTo(ChurchUser, { foreignKey: "userId" });
 }
+
