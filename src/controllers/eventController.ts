615218040ee594c1368a14ec323b4a1c9f2ae40a
LeeMoTakito <110348661+LeeMoTakito@users.noreply.github.com>
2023-05-31 18:33:34 -0700

    cleanup no longer need code

--- src/controllers/eventController.ts
+++ src/controllers/eventController.ts
@@ -6,20 +6,7 @@ import { ChurchUser } from "../models/churchUser";
 import { Op } from "sequelize";
 
 export const getAllEvents: RequestHandler = async (req, res, next) => {
-  // try {
-  //   const events = await Event.findAll({
-  //     include: [
-  //       {
-  //         model: Church,
-  //         include: [ChurchUser],
-  //       },
-  //     ],
-  //   });
-  //   res.json(events);
-  // } catch (error: any) {
-  //   res.status(500).json({ error: error.message });
-  // }
-
+ 
   try {
     let events: Event[] = await Event.findAll({
       include: [
@@ -47,26 +34,7 @@ export const getAllEvents: RequestHandler = async (req, res, next) => {
 };
 
 export const getEvent: RequestHandler = async (req, res, next) => {
-  // try {
-  //   const eventId = req.params.eventId;
-  //   const event = await Event.findByPk(eventId, {
-  //     include: [
-  //       {
-  //         model: Church,
-  //         include: [ChurchUser],
-  //       },
-  //     ],
-  //   });
-
-  //   if (event) {
-  //     res.json(event);
-  //   } else {
-  //     res.status(404).json({ error: "Event not found" });
-  //   }
-  // } catch (error: any) {
-  //   res.status(500).json({ error: error.message });
-  // }
-
+ 
   try {
     const eventId = req.params.eventId;
 
@@ -97,27 +65,7 @@ export const getEvent: RequestHandler = async (req, res, next) => {
 };
 
 export const getTenEvents: RequestHandler = async (req, res, next) => {
-  // try {
-  //   const eventId = req.params.eventId;
-  //   const event = await Event.findByPk(eventId, {
-  //     limit: 10,
-  //     offset: 0,
-  //     include: [
-  //       {
-  //         model: Church,
-  //         include: [ChurchUser],
-  //       },
-  //     ],
-  //   });
-
-  //   if (event) {
-  //     res.json(event);
-  //   } else {
-  //     res.status(404).json({ error: "Event not found" });
-  //   }
-  // } catch (error: any) {
-  //   res.status(500).json({ error: error.message });
-  // }
+ 
 
   try {
     let events: Event[] = await Event.findAll({
@@ -148,27 +96,7 @@ export const getTenEvents: RequestHandler = async (req, res, next) => {
 };
 
 export const createEvent: RequestHandler = async (req, res, next) => {
-  // try {
-  //   const user: ChurchUser | null = await verifyUser(req);
-  //   if (!user) {
-  //     return res.status(403).send();
-  //   }
-
-  //   const newEvent: Event = req.body;
-  //   const church: Church | null = await Church.findByPk(newEvent.churchId);
-  //   if (!church) {
-  //     return res.status(400).json({ error: "Invalid church ID" });
-  //   }
-
-  //   if (church.userId !== user.userId) {
-  //     return res.status(401).send("Not authorized");
-  //   }
-
-  //   const createdEvent = await Event.create(newEvent);
-  //   res.status(201).json(createdEvent);
-  // } catch (error: any) {
-  //   res.status(500).json({ error: error.message });
-  // }
+ 
   try {
     let user: ChurchUser | null = await verifyUser(req);
     if (!user) {
@@ -218,31 +146,7 @@ export const createEvent: RequestHandler = async (req, res, next) => {
 };
 
 export const updateEvent: RequestHandler = async (req, res, next) => {
-  // try {
-  //   const user: ChurchUser | null = await verifyUser(req);
-  //   if (!user) {
-  //     return res.status(403).send();
-  //   }
 
-  //   const eventId: number = parseInt(req.params.eventId);
-  //   const updatedEvent: Event = req.body;
-  //   const event: Event | null = await Event.findByPk(eventId);
-
-  //   if (!event) {
-  //     return res.status(404).json({ error: "Event not found" });
-  //   }
-
-  //   const church: Church | null = await Church.findByPk(event.churchId);
-  //   if (!church || church.userId !== user.userId) {
-  //     return res.status(401).send("Not authorized");
-  //   }
-
-  //   await Event.update(updatedEvent, { where: { eventId: eventId } });
-  //   const updated = await Event.findByPk(eventId);
-  //   res.json(updated);
-  // } catch (error: any) {
-  //   res.status(500).json({ error: error.message });
-  // }
 
   try {
     let user: ChurchUser | null = await verifyUser(req);
