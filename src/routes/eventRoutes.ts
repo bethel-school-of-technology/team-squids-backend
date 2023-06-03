2c1bc537121c50a0e0f71e855293154f12bf343f
LeeMoTakito <110348661+LeeMoTakito@users.noreply.github.com>
2023-06-03 15:46:01 -0700

    made user event function

--- src/routes/eventRoutes.ts
+++ src/routes/eventRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { createEvent, deleteEvent, getAllEvents, getEvent, getTenEvents, updateEvent } from '../controllers/eventController';
+import { createEvent, deleteEvent, getAllEvents, getEvent, getTenEvents, getUserEvents, updateEvent } from '../controllers/eventController';
 import { searchEvent } from '../controllers/searchController';
 
 const router = Router();
 
 router.get("/", getAllEvents)
 router.get("/:eventId", getEvent)
+router.get("/userevent/:userId", getUserEvents)
 router.get('/searchEvents', getTenEvents)
 router.get('/search/:query', searchEvent)
 router.post("/", createEvent)
