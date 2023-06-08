40288ed2100df417d2cef2b03c8bac2c3b00bb19
akinsman22 <hearttorejoice@gmail.com>
2023-06-08 13:54:30 -0700

    updating church and event search. church works

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -8,19 +8,19 @@ import { Event } from "../models/event";
 export const searchChurch: RequestHandler = async (req, res, next) => {
   // Convert the search query to lowercase
   let query = req.params.query.toLowerCase();
- // Minimum length of the search query
- const minimumQueryLength = 3;
- // Check if the query has fewer characters than the minimum length
- if (query.length < minimumQueryLength) {
-  return res.status(400).json({ error: 'Search query must have at least 3 characters' });
-}
+  // Minimum length of the search query
+  const minimumQueryLength = 3;
+  // Check if the query has fewer characters than the minimum length
+  if (query.length < minimumQueryLength) {
+    return res.status(400).json({ error: 'Search query must have at least 3 characters' });
+  }
   try {
     let resultsDB = await Church.findAll({
       where: {
         [Op.or]: [
-      
+          Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('churchName')), 'LIKE', `%${query.toLowerCase()}%`),
           Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('location')), 'LIKE', `%${query.toLowerCase()}%`),
-          // Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('churchName')), 'LIKE', `%${query.toLowerCase()}%`)
+          Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('denomination')), 'LIKE', `%${query.toLowerCase()}%`),
         ]
       }
 
@@ -43,28 +43,44 @@ export const searchChurch: RequestHandler = async (req, res, next) => {
 export const searchEvent: RequestHandler = async (req, res, next) => {
   // Convert the search query to lowercase
   let query = req.params.query.toLowerCase();
- // Minimum length of the search query
- const minimumQueryLength = 3;
- // Check if the query has fewer characters than the minimum length
- if (query.length < minimumQueryLength) {
-  return res.status(400).json({ error: 'Search query must have at least 3 characters' });
-}
+  // Minimum length of the search query
+  const minimumQueryLength = 3;
+  // Check if the query has fewer characters than the minimum length
+  if (query.length < minimumQueryLength) {
+    return res.status(400).json({ error: 'Search query must have at least 3 characters' });
+  }
   try {
     let resultsDB = await Event.findAll({
+      // include: [{
+      //   model:Church,
+      //   where: {
+      //     churchName:{ [Op.like]: `%${query}%` }
+      //     // Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('churchName')), 'LIKE', `%${query.toLowerCase()}%`)
+      //   }
+      // }],
       where: {
         [Op.or]: [
-      
           Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('location')), 'LIKE', `%${query.toLowerCase()}%`),
-          // Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('churchName')), 'LIKE', `%${query.toLowerCase()}%`)
+          Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('eventType')), 'LIKE', `%${query.toLowerCase()}%`),
+         
+          //  Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('date')), 'LIKE', `%${query.toLowerCase()}%`),
+
         ]
-      }
+      },
+      
 
     });
 
     resultsDB = resultsDB.map((event) => {
+      // if (event.Church.churchName === "string") {
+      //   event.churchId = JSON.parse(event.churchId);
+      // }
       if (typeof event.location === "string") {
         event.location = JSON.parse(event.location);
       }
+      // if (typeof event.eventType === "string") {
+      //   event.eventType = JSON.parse(event.eventType);
+      // }
       return event;
     });
 
